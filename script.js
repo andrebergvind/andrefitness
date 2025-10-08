6e481971e745ee3f46328c1b83d54c5a0df5300c
André Bergvind <andrebergvind@gmail.com>
2025-10-08 07:55:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple form handler
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for your message! I will get back to you soon.');
+  this.reset();
+});
